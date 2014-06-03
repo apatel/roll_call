a777b6f2201ff1185a52e64284cf1774b57e40d1
Anita Patel <anita@anitapatel.com>
2014-06-02 21:14:23 -0400

    beginning packages and individuals

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rollcall::Application.routes.draw do
+  resources :individuals
+
+  resources :packages
+  
+  resources :dashboard do
+    collection do
+      get 'packages'
+    end
+  end
+
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'dashboard#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
